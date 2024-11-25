f8d879dbd042d71aaa93c5868fe6be09371fe246
Jane0056 <159496358+Jane0056@users.noreply.github.com>
2024-11-25 04:55:40 +0100

    Added refreshWeather function to display weather details

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("hello");
+// Function to refresh weather details on the page
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity-details");
+  let windSpeedElement = document.querySelector("#speed-details");
+  let timeElement = document.querySelector("#time");
+  let iconElement = document.querySelector("#icon");
+
+  
